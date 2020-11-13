207b47a0eeaa43cd1a08a2fb15b39a9aff774eb9
HuffDaddy0 <ty.huff24@gmail.com>
2020-11-13 16:02:33 -0500

    Notes Controller skeleton

--- Backend/app/controllers/notes_controller.rb
+++ Backend/app/controllers/notes_controller.rb
@@ -1,2 +1,22 @@
 class NotesController < ApplicationController
+
+    def index
+
+    end
+
+    def show
+
+    end
+
+    def create
+
+    end
+
+    def update
+
+    end
+
+    def destroy
+        
+    end
 end
