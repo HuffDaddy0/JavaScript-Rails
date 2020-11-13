7e5d32463ebe7c5721c03ac11c322b318446afca
HuffDaddy0 <ty.huff24@gmail.com>
2020-11-13 15:36:11 -0500

    resource generated languages and notes

--- Backend/db/seeds.rb
+++ Backend/db/seeds.rb
@@ -5,3 +5,11 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+ruby = Language.create(name: 'Ruby', category: 'Object Oriented')
+js = Language.create(name: 'JavaScript', category: 'Object Oriented')
+python = Language.create(name: 'Python', category: 'Object Oriented')
+
+ruby.notes.create(title: 'variables', body: 'assign variables like this: variable_name = "this is a variable, its type is string"')
+js.notes.create(title: "variables", body: 'JS variables use the keyword let, const, or var. They each have different functionality. ex: const variableName = 14')
+python.notes.create(title:"variables", body: "I dont know how to write variables in Python!! Check a later note :)")
