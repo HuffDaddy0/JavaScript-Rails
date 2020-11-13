35b0a207e2f3d0b14a89818d1207570f3cd4f153
HuffDaddy0 <ty.huff24@gmail.com>
2020-11-13 16:01:11 -0500

    set up CRUD languages controller

--- Backend/app/controllers/languages_controller.rb
+++ Backend/app/controllers/languages_controller.rb
@@ -1,2 +1,34 @@
 class LanguagesController < ApplicationController
+    before_action :find_language, only: [:show]
+
+    def index
+        languages = Language.all
+        render json: languages, include: [:notes]
+    end
+
+    def show
+        # language = Language.find_by(id: params[:id])
+
+        render json: @language, include: [:notes]
+    end
+
+    def create
+        language.create(language_params)
+    end
+
+    def edit
+        language.update(language_params)
+    end
+
+    def destroy
+        language.destroy
+    end
+
+    private
+    def language_params
+        params.require(:language).permit(:name, :category)
+    end
+    def find_language
+        @language = Language.find_by(id: params[:id])
+    end
 end
